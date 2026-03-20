33f4b2abaa4aa7a05387debad54b7942c1330bf1
jayaprakash256006-coder <jayaprakash256006@gmail.com>
2026-03-20 14:04:53 +0530

    UC1

---
+++ BookMyStayApp/src/UC1HotelBookingApp.java
@@ -0,0 +1,6 @@
+public class UC1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Hotel Booking Management System");
+        System.out.println("System Initialized Successfully.");
+    }
+}
