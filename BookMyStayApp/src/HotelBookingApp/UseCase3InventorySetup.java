066699c351555df061d5685214c4e517bed08477
jayaprakash256006-coder <jayaprakash256006@gmail.com>
2026-03-20 14:43:11 +0530

    UC3

---
+++ BookMyStayApp/src/HotelBookingApp/UseCase3InventorySetup.java
@@ -0,0 +1,25 @@
+package HotelBookingApp;
+
+import HotelBookingApp.RoomInventory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UseCase3InventorySetup {
+    public static void main(String[] args) {
+        RoomInventory inventory = new RoomInventory();
+
+        System.out.println("Initial Inventory:");
+        for (Map.Entry<String, Integer> entry : inventory.getRoomAvailability().entrySet()) {
+            System.out.println(entry.getKey() + " Rooms Available: " + entry.getValue());
+        }
+
+        inventory.updateAvailability("Single", 4);
+
+        System.out.println();
+        System.out.println("Updated Inventory:");
+        for (Map.Entry<String, Integer> entry : inventory.getRoomAvailability().entrySet()) {
+            System.out.println(entry.getKey() + " Rooms Available: " + entry.getValue());
+        }
+    }
+}
